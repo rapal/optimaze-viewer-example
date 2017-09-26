c9beab06bd532b847238230eb491cb059915785b
Unknown <joakim.gunst@rapal.fi>
2017-09-26 14:52:59 +0300

    Initial version

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # optimaze-viewer-example
-Example application for the `optimaze-viewer` library. Written in TypeScript and using Webpack.
+
+This is an example application for the [`optimaze-viewer`](https://github.com/rapal/optimaze-viewer) library. 
+It is written in TypeScript and uses Webpack for bundling.
+
+Note: The API which this application uses is not yet published but will be published later in 2017.
+
+## Usage
+
+1. Run `yarn` to install dependencies
+2. Run `yarn start` to start dev server
+3. Alternatively, run `yarn build` and open `index.html`
---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+var path = require("path");
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+module.exports = {
+  devtool: "inline-source-map",
+  entry: "./src/app.ts",
+  output: {
+    path: path.resolve("build"),
+    filename: "bundle.js"
+  },
+  plugins: [
+    new ExtractTextPlugin({
+      filename: "bundle.css"
+    })
+  ],
+  resolve: {
+    modules: [path.resolve("node_modules")],
+    extensions: [".ts", ".js"]
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.ts$/,
+        include: path.resolve("src"),
+        loader: "ts-loader"
+      },
+      {
+        test: /\.css$/,
+        loader: ExtractTextPlugin.extract({
+          use: "css-loader"
+        })
+      },
+      {
+        test: /\.png$/,
+        loader: "file-loader"
+      }
+    ]
+  }
+};
